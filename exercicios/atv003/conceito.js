94acf802ddc30bb42ebd1e6cad0eff6216471caa
Ismael de Araujo <araujoismael0982@gmail.com>
2025-06-30 22:39:37 -0300

    .

---
+++ exercicios/atv003/conceito.js
@@ -0,0 +1,6 @@
+let frutas = ["apple", "banana", "pineapple", "strawberry"]
+
+// alert(frutas.indexOf("apple"))
+
+alert(frutas[5])
+alert(frutas.length)
--- exercicios/atv003/script.js
+++ exercicios/atv003/script.js
@@ -3,12 +3,17 @@ function adicionarTarefa() {
 
     let inputTarefa = document.getElementById("inputTarefa")
     let tarefa = inputTarefa.value.trim()
+    let mensagem = document.getElementById("mensagem")
 
     if (tarefa == "" ) {
-        window.alert("Insira sua tarefa para prosseguir!")
+        
+        let mensagemError = "Digite uma tarefa para prosseguir!"
+        mensagem.textContent = mensagemError
+        mensagem.style.color = "red"
     } else {
-        let mensagem = "Tarefa adicionada com sucesso!"
-        document.getElementById("mensagem").textContent = mensagem
+        let mensagemSucesso = "Tarefa adicionada com sucesso!"
+        mensagem.textContent = mensagemSucesso
+        mensagem.style.color = "green"
         let listaTarefas = document.getElementById("listaTarefas")
         let novaTarefa = document.createElement("li")
         novaTarefa.textContent = tarefa
