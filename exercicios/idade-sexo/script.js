fb0747154797c8eeff80f84ee777d12b97a3122c
Mael <araujoismael0982@gmail.com>
2025-08-16 13:07:47 -0300

    atualização no exercicio idade-sexo

--- exercicios/idade-sexo/script.js
+++ exercicios/idade-sexo/script.js
@@ -1,11 +1,21 @@
+let button = document.getElementById("button")
 button.addEventListener("click", Verify)
 
 function Verify() {
+
+    let birthValue = document.getElementById("birth").value.trim()
     let date = new Date()
     let year = date.getFullYear()
-    let birth = document.getElementById("birth").value
-    let button = document.getElementById("button")
     let msg = document.getElementById("msg")
-    let age = year - birth
-    msg.innerHTML = `Your have ${age} years`
+    let sex = document.getElementsByName("sexo")
+
+    if (birthValue.length === 0 || Number(birthValue) > year || Number.isNaN(birthValue))  {
+        msg.style.display = "block"
+    } else {
+        let birth = Number(birthValue)
+        let age = year - birth
+        msg.innerHTML = `You have ${age} years`
+        msg.style.display = "block"
+    }
+
 }
